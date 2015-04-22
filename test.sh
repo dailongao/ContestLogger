c201d0f739b07eccbd5884777ca00b16b40e7a4a
zYc <zhouyuchen.zyc@gmail.com>
2015-04-23 06:14:08 +0800

    test.sh

---
+++ test.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+db='zjp2015.db'
+echo $db
+rm $db
+
+sqlite3 $db < new.sql
+
+sqlite3 $db <<EOF
+insert into teams values
+(1,"team1","school1","a1","a2","a3"),
+(2,"team2","school2","b1","b2","b3"),
+(3,"team3","school3","c1","c2","c3"),
+(4,"team4","school3","d1","d2","d3");
+EOF
+
+
+
